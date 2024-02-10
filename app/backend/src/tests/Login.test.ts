9d0ad2af1181519407431bc7b9bb819e0ce1e93b
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-09 23:52:14 -0300

    test: stub bcrypt compare in login test & describe post login

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -6,6 +6,7 @@ import SUserModel from '../database/models/SUserModel';
 import UserMock from './mocks/User.mock';
 import { app } from '../app';
 import LoginMock from './mocks/Login.mock';
+import * as bcrypt from 'bcryptjs';
 
 chai.use(chaiHttp);
 
@@ -14,16 +15,19 @@ const { expect } = chai;
 describe('Login Test', () => {
   beforeEach(sinon.restore);
 
-  it('should login with success and return a token', async () => {
-    const mockUser = SUserModel.build(UserMock.users[1]);
-    sinon.stub(SUserModel, 'findOne').resolves(mockUser);
-
-    const { status, body } = await chai
-      .request(app)
-      .post('/login')
-      .send(LoginMock.body);
-
-    expect(status).to.be.equal(200);
-    expect(body).to.have.property('token');
-  });
+  describe('POST /login', () => {
+    it('should login with success and return a token', async () => {
+      const mockUser = SUserModel.build(UserMock.users[1]);
+      sinon.stub(SUserModel, 'findOne').resolves(mockUser);
+      sinon.stub(bcrypt, 'compare').resolves(true);
+  
+      const { status, body } = await chai
+        .request(app)
+        .post('/login')
+        .send(LoginMock.body);
+  
+      expect(status).to.be.equal(200);
+      expect(body).to.have.property('token');
+    });
+  })
 });
