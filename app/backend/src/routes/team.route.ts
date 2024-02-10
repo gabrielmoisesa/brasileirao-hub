430e8461dfb28b527b9ef5a9a81f93cb677fb89f
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-09 23:26:00 -0300

    refactor: team route request params to match login route pattern

--- app/backend/src/routes/team.route.ts
+++ app/backend/src/routes/team.route.ts
@@ -1,11 +1,11 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import TeamController from '../controllers/TeamController';
 
 const teamController = new TeamController();
 
 const teamRoutes = Router();
 
-teamRoutes.get('/', (req: Request, res: Response) => teamController.getAll(req, res));
-teamRoutes.get('/:id', (req: Request, res: Response) => teamController.getById(req, res));
+teamRoutes.get('/', (req, res) => teamController.getAll(req, res));
+teamRoutes.get('/:id', (req, res) => teamController.getById(req, res));
 
 export default teamRoutes;
