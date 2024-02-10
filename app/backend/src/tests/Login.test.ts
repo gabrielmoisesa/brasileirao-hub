4bc7b33f29278f57e16bc71cc28690766e18c1ef
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-10 02:09:20 -0300

    req 9(tdd): post /login validations test

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -20,14 +20,91 @@ describe('Login Test', () => {
       const mockUser = SUserModel.build(UserMock.users[1]);
       sinon.stub(SUserModel, 'findOne').resolves(mockUser);
       sinon.stub(bcrypt, 'compare').resolves(true);
-  
+
       const { status, body } = await chai
         .request(app)
         .post('/login')
         .send(LoginMock.body);
-  
+
       expect(status).to.be.equal(200);
       expect(body).to.have.property('token');
     });
-  })
+
+    describe('Validation', () => {
+      const invalidFormatMessage = { message: 'Invalid email or password' };
+
+      describe('Email', () => {
+        const invalidEmails = [
+          '@example.com',
+          'mail@example',
+          'example@.com',
+          'example.example.com',
+        ];
+        const password = '123456';
+
+        invalidEmails.forEach((invalidEmail) => {
+          it(`should return unauthorized if email "${invalidEmail}" has invalid format`, async () => {
+            const mockUser = SUserModel.build(UserMock.users[1]);
+            sinon.stub(SUserModel, 'findOne').resolves(mockUser);
+            sinon.stub(bcrypt, 'compare').resolves(true);
+
+            const { status, body } = await chai
+              .request(app)
+              .post('/login')
+              .send({
+                email: invalidEmail,
+                password,
+              });
+
+            expect(status).to.be.equal(401);
+            expect(body).to.be.deep.equal(invalidFormatMessage);
+          });
+        });
+
+        it('should return unauthorized if email is not found', async () => {
+          sinon.stub(SUserModel, 'findOne').resolves(null);
+
+          const { status, body } = await chai
+            .request(app)
+            .post('/login')
+            .send(LoginMock.body);
+
+          expect(status).to.be.equal(401);
+          expect(body).to.be.deep.equal(invalidFormatMessage);
+        });
+      });
+
+      describe('Password', () => {
+        const email = LoginMock.body.email;
+
+        it('should return unauthorized if password has invalid format', async () => {
+          const mockUser = SUserModel.build(UserMock.users[1]);
+          sinon.stub(SUserModel, 'findOne').resolves(mockUser);
+          sinon.stub(bcrypt, 'compare').resolves(true);
+
+          const { status, body } = await chai.request(app).post('/login').send({
+            email,
+            password: '123',
+          });
+
+          expect(status).to.be.equal(401);
+          expect(body).to.be.deep.equal(invalidFormatMessage);
+        });
+
+        it('should return unauthorized if password is incorrect', async () => {
+          const mockUser = SUserModel.build(UserMock.users[1]);
+          sinon.stub(SUserModel, 'findOne').resolves(mockUser);
+          sinon.stub(bcrypt, 'compare').resolves(false);
+
+          const { status, body } = await chai
+            .request(app)
+            .post('/login')
+            .send(LoginMock.body);
+
+          expect(status).to.be.equal(401);
+          expect(body).to.be.deep.equal(invalidFormatMessage);
+        });
+      });
+    });
+  });
 });
