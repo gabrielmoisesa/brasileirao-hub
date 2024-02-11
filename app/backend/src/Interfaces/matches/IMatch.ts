beeebb11f3939334ee64b538ded4e9fd70297a42
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-11 00:30:46 -0300

    req 13: create match interface and migration

---
+++ app/backend/src/Interfaces/matches/IMatch.ts
@@ -0,0 +1,9 @@
+import { Identifiable } from '../../types';
+
+export interface IMatch extends Identifiable {
+  homeTeamId: number;
+  homeTeamGoals: number;
+  awayTeamId: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
