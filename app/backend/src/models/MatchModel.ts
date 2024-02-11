40cf5432dc1072b734f568973615ba8366b927d5
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-11 02:10:41 -0300

    req 15: add includes in match model find all

--- app/backend/src/models/MatchModel.ts
+++ app/backend/src/models/MatchModel.ts
@@ -1,28 +1,29 @@
 import { IMatch } from '../Interfaces/matches/IMatch';
 import SMatchModel from '../database/models/SMatchModel';
 import { IMatchModel } from '../Interfaces/matches/IMatchModel';
+import STeamModel from '../database/models/STeamModel';
 
 export default class MatchModel implements IMatchModel {
   private model = SMatchModel;
 
   async findAll(): Promise<IMatch[]> {
-    const data = await this.model.findAll();
-    return data.map(
-      ({
-        id,
-        homeTeamId,
-        homeTeamGoals,
-        awayTeamId,
-        awayTeamGoals,
-        inProgress,
-      }) => ({
-        id,
-        homeTeamId,
-        homeTeamGoals,
-        awayTeamId,
-        awayTeamGoals,
-        inProgress,
-      }),
-    );
+    const matches = this.model.findAll({
+      include: [
+        {
+          model: STeamModel,
+          as: 'homeTeam',
+          foreignKey: 'homeTeamId',
+          attributes: ['teamName'],
+        },
+        {
+          model: STeamModel,
+          as: 'awayTeam',
+          foreignKey: 'awayTeamId',
+          attributes: ['teamName'],
+        },
+      ],
+    });
+
+    return matches;
   }
 }
