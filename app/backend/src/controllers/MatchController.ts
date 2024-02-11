d212c74dd655627094080d9f38e234d2013db961
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-11 02:59:30 -0300

    req 16: in progress query param filter for get all matches

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -6,7 +6,8 @@ export default class MatchController {
   constructor(private matchService = new MatchService()) {}
 
   public async getAll(req: Request, res: Response) {
-    const { status, data } = await this.matchService.getAll();
+    const inProgress = typeof req.query.inProgress === 'string' ? req.query.inProgress : undefined;
+    const { status, data } = await this.matchService.getAll(inProgress);
     res.status(httpMap(status)).json(data);
   }
 }
--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -6,8 +6,16 @@ import { IMatch } from '../Interfaces/matches/IMatch';
 export default class MatchService {
   constructor(private matchModel: IMatchModel = new MatchModel()) {}
 
-  public async getAll(): Promise<ServiceResponse<IMatch[]>> {
+  public async getAll(inProgress?: string): Promise<ServiceResponse<IMatch[]>> {
     const matches = await this.matchModel.findAll();
+    if (inProgress === 'true' || inProgress === 'false') {
+      return {
+        status: 'OK',
+        data: matches.filter(
+          (match) => match.inProgress === JSON.parse(inProgress),
+        ),
+      };
+    }
     return { status: 'OK', data: matches };
   }
 }
