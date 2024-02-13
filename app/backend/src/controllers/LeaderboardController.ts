8507e8ba8f51e2f8c203d588c5a738804d6b9fcf
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-12 21:01:25 -0300

    refactor: change leaderboard service use in service controller

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
+import LeaderboardService from '../services/LeaderboardService';
 import httpMap from '../utils/httpMap';
-import HomeService from '../services/leaderboard/HomeService';
 
 export default class LeaderboardController {
-  constructor(private homeService = new HomeService()) {}
+  constructor(private leaderboardService = new LeaderboardService()) {}
 
   public async getAllHome(req: Request, res: Response) {
-    const { status, data } = await this.homeService.getAll();
+    const { status, data } = await this.leaderboardService.getAllHome();
     res.status(httpMap(status)).json(data);
   }
 }
