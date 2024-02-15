35fecf48e99566d9665bbd158ad089045685a0c7
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-14 22:29:49 -0300

    build: set frontend port and host for vite running with docker & change frontend node version in docker container

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -5,4 +5,8 @@ import envCompatible from 'vite-plugin-env-compatible'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), envCompatible()],
+  server: {
+    host: true,
+    port: 3000
+  }
 })
