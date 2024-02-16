d33d76e841f622dfe74da2d683668edb8b942bdc
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-15 21:03:33 -0300

    style: format add new match btn

--- app/frontend/src/components/AddNewMatchBtn.jsx
+++ app/frontend/src/components/AddNewMatchBtn.jsx
@@ -2,10 +2,7 @@ import { Link } from 'react-router-dom';
 
 const AddNewMatchBtn = () => (
   <div>
-    <Link
-      className="text-slate-800 underline font-bold"
-      to="/matches/settings"
-    >
+    <Link className="text-slate-800 underline font-bold" to="/matches/settings">
       + Adicionar nova partida
     </Link>
   </div>
