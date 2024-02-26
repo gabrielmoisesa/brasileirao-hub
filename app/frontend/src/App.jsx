cd7bf58553460a03d5f842bf2be7c6c8f12fb100
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-26 03:55:08 -0300

    feat: wrap routes in layout route

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -4,15 +4,18 @@ import Leaderboard from './pages/Leaderboard';
 import Games from './pages/Games';
 import Login from './pages/Login';
 import { Navigate } from 'react-router-dom';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route exact path='/' element={<Navigate to='/leaderboard' />} />
-      <Route path='matches/settings' element={<MatchSettings />} />
-      <Route path='/leaderboard' element={<Leaderboard />} />
-      <Route path='/matches' element={<Games />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Layout />}>
+        <Route exact path='/' element={<Navigate to='/leaderboard' />} />
+        <Route path='matches/settings' element={<MatchSettings />} />
+        <Route path='/leaderboard' element={<Leaderboard />} />
+        <Route path='/matches' element={<Games />} />
+        <Route path='/login' element={<Login />} />
+      </Route>
     </Routes>
   );
 }
