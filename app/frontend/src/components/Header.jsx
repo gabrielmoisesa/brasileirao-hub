49148808acc9ede2311602d9de23b9e7a9c94bc2
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-02-27 01:32:14 -0300

    feat: change header style and remove matches and leaderboard navigation & background color

--- app/frontend/src/components/Header.jsx
+++ app/frontend/src/components/Header.jsx
@@ -1,19 +1,11 @@
-import PropTypes from 'prop-types';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { exitToAppImg, positiveLogo } from '../images';
-import LeaderboardBtn from './LeaderboardBtn';
-import MatchesBtn from './MatchesBtn';
 import LoginBtn from './LoginBtn';
 import { useEffect, useState } from 'react';
 
-const Header = ({
-  FirstNavLink = LeaderboardBtn,
-  SecondNavLink = MatchesBtn,
-  ThirdNavLink = LoginBtn,
-}) => {
+const Header = () => {
   const [logged, setLogin] = useState(false);
   const navigate = useNavigate();
-  const pathName = useLocation().pathname;
 
   useEffect(() => {
     const token = localStorage.getItem('token') || false;
@@ -27,42 +19,28 @@ const Header = ({
     navigate('/leaderboard');
   };
 
-  const activePageClass = 'underline';
-
   return (
-    <header className='flex items-center text-white'>
-      <div className='h-24 flex items-center ml-4 mr-1'>
+    <header className='flex items-center place-content-between h-24 px-10 bg-white'>
+      <div className='flex items-center space-x-1'>
+        <span className='text-2xl'>Brasileirão Hub</span>
         <img
           src={positiveLogo}
           alt='Brasileirao Positive Logo'
-          className='h-24 w-28'
+          className='h-14'
         />
       </div>
-      <span className='angled-header'></span>
-      <div className='bg-blue-800 h-24 w-full pl-4 flex items-center space-x-7'>
-        <FirstNavLink
-          className={pathName === '/leaderboard' && activePageClass}
-        />
-        <SecondNavLink
-          className={pathName.includes('/matches') && activePageClass}
-        />
+      <div>
         {logged ? (
           <button type='button' onClick={() => logoff()}>
             Sair
             <img src={exitToAppImg} alt='Sair do aplicativo' />
           </button>
         ) : (
-          <ThirdNavLink className={pathName === '/login' && activePageClass} />
+          <LoginBtn />
         )}
       </div>
     </header>
   );
-};
-
-Header.propTypes = {
-  FirstNavLink: PropTypes.elementType,
-  SecondNavLink: PropTypes.elementType,
-  ThirdNavLink: PropTypes.elementType,
 };
 
 export default Header;
