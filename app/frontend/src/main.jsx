0f95fab2204cd38fe3f30b6be4334438d225b037
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-03-02 22:24:53 -0300

    feat: implement global provider in main

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import GlobalProvider from './context/GlobalProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GlobalProvider>
   </React.StrictMode>
 );
