89f7c54ad6b541978a72a6d74f72e8fcf5d2c91a
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-03-02 22:20:23 -0300

    feat: create initial global context with teams request

---
+++ app/frontend/src/context/GlobalProvider.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react';
+import { requestData } from '../services/requests';
+import GlobalContext from './GlobalContext';
+
+const GlobalProvider = ({ children }) => {
+  const [teams, setTeams] = useState([]);
+
+  const getTeams = (endpoint) =>
+    requestData(endpoint)
+      .then((response) => setTeams(response))
+      .catch((error) => console.log(error));
+
+  useEffect(() => {
+    const apiTeams = '/teams';
+    getTeams(apiTeams);
+  });
+
+  const contextValue = {
+    teams,
+  };
+
+  return (
+    <GlobalContext.Provider value={contextValue}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalProvider;
